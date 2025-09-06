4f382ede52d8e15919d85638309fc113520aecc8
rafabcanedo <rafaitecanedo@gmail.com>
2025-09-06 17:59:23 -0300

    created migrations

--- internal-system-backend/db/migrations/20250906194539_create-users.ts
+++ internal-system-backend/db/migrations/20250906194539_create-users.ts
@@ -3,11 +3,11 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
-    table.text('name').notNullable()
-    table.text('email').notNullable().unique()
-    table.text('password').notNullable()
-    table.text('telefone').nullable()
-    table.timestamps(true, true)
+    table.string('name', 30).notNullable()
+    table.string('email').notNullable().unique()
+    table.string('password', 20).notNullable()
+    table.string('phone', 20).nullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
