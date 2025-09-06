05cba514e5b8048c3748529ce483b7fa2d10de45
rafabcanedo <rafaitecanedo@gmail.com>
2025-09-06 17:10:53 -0300

    initial commit

---
+++ internal-system-backend/src/database.ts
@@ -0,0 +1,17 @@
+import setupKnex from 'knex'
+import type { Knex } from 'knex'
+
+// database config
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
